b6dba96c887cd73d20f1f6296fc43409f39835be
Sergey Korobka <nejdancs@gmail.com>
2022-06-18 00:33:49 +0300

    fix key

--- src/js/fetch-service.js
+++ src/js/fetch-service.js
@@ -1,25 +1,7 @@
 import axios from 'axios';
 
 const API_URL = 'https://pixabay.com/api/';
-const KEY = '2788855-96adad5e55b58177e65876141';
-
-// export default async function getImages(query, page) {
-//   const BASE_SEARH_PARAMS = {
-//     key: KEY,
-//     image_type: 'photo',
-//     orientation: 'horizontal',
-//     safesearch: true,
-//     per_page: 40,
-//     page,
-//     q: query,
-//   };
-
-//   const response = await axios.get(API_URL, {
-//     params: BASE_SEARH_PARAMS,
-//   });
-
-//   return response.data;
-// }
+const KEY = '27888557-96adad5e55b58177e65876141';
 
 export default function getImages(query, page) {
   const BASE_SEARH_PARAMS = {
