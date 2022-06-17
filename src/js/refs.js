cbb0f5b19c1fd506c92505049b18b1b94e5ceb45
Sergey Korobka <nejdancs@gmail.com>
2022-06-17 18:47:52 +0300

    fix refs

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,6 +1,8 @@
-export default refs = {
+const refs = {
   form: document.getElementById('search-form'),
   gallery: document.querySelector('.gallery'),
   loadBtn: document.querySelector('.load-more'),
   spinner: document.querySelector('.spinner'),
 };
+
+export default refs;
